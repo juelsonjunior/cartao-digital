28b0d3e4b6e8992ce44ea71ec0d741d84ddbd48d
juelsonjunior <juelsonjunior27@gmail.com>
2025-03-08 22:19:30 +0100

    feat:Validando o contexto no formulario

--- src/components/Formulario.tsx
+++ src/components/Formulario.tsx
@@ -1,4 +1,31 @@
+"use client";
+import { useCardContext } from "@/context/cardContext";
+
 export const Formulario = () => {
+  const {
+    numberCard,
+    setNumberCard,
+    setNameCard,
+    expireCard,
+    setExpireCard,
+    setCodigoCard,
+  } = useCardContext();
+
+  const handleCardNumberChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    const value = e.target.value.replace(/\D/g, ""); // Remove caracteres não numéricos
+    const limitedValue = value.slice(0, 16); // Limita a 12 números
+    const formattedValue = limitedValue.replace(/(\d{4})(?=\d)/g, "$1 "); // Insere espaço a cada 4 dígitos
+    setNumberCard(formattedValue); // Atualiza o estado já formatado
+  };
+
+  const handleExpireDateChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    const value = e.target.value.replace(/\D/g, ""); // Remove caracteres não numéricos
+    const formattedValue = value
+      .replace(/^(\d{2})(\d{1,2})/, "$1/$2")
+      .slice(0, 5); // Formata como MM/AA
+    setExpireCard(formattedValue); // Atualiza o estado já formatado
+  };
+
   return (
     <form className="flex flex-col gap-8">
       <div className="flex flex-col gap-2">
@@ -8,6 +35,8 @@ export const Formulario = () => {
         <input
           type="text"
           placeholder="Numero do cartão"
+          value={numberCard}
+          onChange={handleCardNumberChange}
           className="outline-none border-1 border-gray-400 px-2 py-3 bg-slate-950 text-xs rounded-xs"
         />
       </div>
@@ -19,6 +48,7 @@ export const Formulario = () => {
         <input
           type="text"
           placeholder="Nome do titular"
+          onChange={(e) => setNameCard(e.target.value)}
           className="outline-none border-1 border-gray-400 px-2 py-3 bg-slate-950 text-xs rounded-xs"
         />
       </div>
@@ -26,11 +56,13 @@ export const Formulario = () => {
       <div className="flex justify-between gap-2">
         <div className="flex flex-col gap-2">
           <label htmlFor="" className="text-xs">
-            Validade
+            Expire
           </label>
           <input
             type="text"
             placeholder="data"
+            value={expireCard}
+            onChange={handleExpireDateChange}
             className="outline-none border-1 border-gray-400 px-2 py-3 bg-slate-950 text-xs rounded-xs"
           />
         </div>
@@ -41,6 +73,7 @@ export const Formulario = () => {
           <input
             type="text"
             placeholder="Codigo"
+            onChange={(e) => setCodigoCard(Number(e.target.value))}
             className="outline-none border-1 border-gray-400 px-2 py-3 bg-slate-950 text-xs rounded-xs"
           />
         </div>
