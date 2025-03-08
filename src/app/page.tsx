3d13021ca58cbb8afeebe6bae075cca191774a6b
juelsonjunior <juelsonjunior27@gmail.com>
2025-03-08 22:19:47 +0100

    feat:Chamando o provider

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,32 +1,38 @@
 import { BackCard } from "@/components/Back-card";
 import { Formulario } from "@/components/Formulario";
 import { FrontCard } from "@/components/Front-card";
+import { CardProvider } from "@/context/cardContext";
 import { IconRefresh } from "@tabler/icons-react";
-import { IconShieldCheckFilled } from '@tabler/icons-react';
+import { IconShieldCheckFilled } from "@tabler/icons-react";
 
 export default function Home() {
   return (
-    <div className="bg-slate-950 flex justify-center items-center w-lvw h-lvh">
-      <div className="bg-slate-900 p-5 rounded-sm flex flex-col gap-10">
-        <div className="flex justify-between gap-10">
-          <div className="w-full">
-            <Formulario />
-          </div>
-          <div className="flex flex-col gap-3">
-            <FrontCard />
-            <div className="flex justify-center items-center flex-col gap-2">
-              <IconRefresh stroke={1} className="cursor-pointer"/>
-              <div className="flex gap-1 justify-center items-center">
-                <IconShieldCheckFilled stroke={2} color="green" size={15}/>
-                <p className="text-xs">Seus dados estão protegidos</p>
+    <CardProvider>
+      <div className="bg-slate-950 flex justify-center items-center w-lvw h-lvh">
+        <div className="bg-slate-900 p-5 rounded-sm flex flex-col gap-10">
+          <div className="flex justify-between gap-10">
+            <div className="w-full">
+              <Formulario />
+            </div>
+            <div className="flex flex-col gap-3">
+              <FrontCard />
+              <div className="flex justify-center items-center flex-col gap-2">
+                <IconRefresh stroke={1} className="cursor-pointer" />
+                <div className="flex gap-1 justify-center items-center">
+                  <IconShieldCheckFilled stroke={2} color="green" size={15} />
+                  <p className="text-xs">Seus dados estão protegidos</p>
+                </div>
               </div>
             </div>
           </div>
+          <button
+            type="submit"
+            className="w-full bg-purple-600 py-3 cursor-pointer"
+          >
+            Adicionar cartão
+          </button>
         </div>
-        <button type="submit" className="w-full bg-purple-600 py-3 cursor-pointer">
-          Adicionar cartão
-        </button>
       </div>
-    </div>
+    </CardProvider>
   );
 }
